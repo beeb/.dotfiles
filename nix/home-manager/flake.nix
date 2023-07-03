ca4edb6621e2e7b6d8552cc762e16ed2bb4cd877
beeb <703631+beeb@users.noreply.github.com>
2023-07-03 16:14:31 +0200

    add configuration for macbook

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -13,13 +13,9 @@
   };
 
   outputs = { nixpkgs, home-manager, rust-overlay, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      homeConfigurations.valentin = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      homeConfigurations."valentin@DESKTOP-SNQ577U" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
@@ -33,6 +29,16 @@
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+      };
+      homeConfigurations."valentin@valentins-macbook-pro.home" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-darwin";
+        modules = [
+          ./home.nix
+          inputs.sops-nix.homeManagerModules.sops
+          ({ ... }: {
+            nixpkgs.overlays = [ rust-overlay.overlays.default ];
+          })
+        ];
       };
     };
 }

c3c078f4d5f259f48b4973f8a51c5690850d2b32
beeb <703631+beeb@users.noreply.github.com>
2023-07-06 13:43:06 +0200

    fix failing build on mac

--- nix/home-manager/macbook.nix
+++ nix/home-manager/macbook.nix
@@ -2,6 +2,8 @@
 {
   home.username = "valentin";
   home.homeDirectory = "/Users/valentin";
+  # Fails to build, see https://github.com/NixOS/nix/issues/8485
+  manual.manpages.enable = false;
 
   sops = {
     age.keyFile = "/Users/valentin/Library/Application Support/sops/age/keys.txt";

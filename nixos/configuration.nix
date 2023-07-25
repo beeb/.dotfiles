78357669c298984fb916d6577c66c0e46ee778bd
beeb <703631+beeb@users.noreply.github.com>
2023-07-25 16:35:41 +0200

    Revert "Revert "make home manager a nix module""
    This reverts commit d190ba99af23251df86b8adaeff37aec0a14fe08.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,21 @@
-{ ... }:
+{ inputs, outputs, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/virtualbox-demo.nix> ];
+  imports = [
+    <nixpkgs/nixos/modules/installer/virtualbox-demo.nix>
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
   time.timeZone = "Europe/Zurich";
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      demo = import ../home-manager/virtualbox.nix; # imports all the stuff
+    };
+  };
+
   nixpkgs.hostPlatform = "x86_64-linux";
 }

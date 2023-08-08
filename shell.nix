645654eafd6dcc7759339f0a3608dca7be3d5856
beeb <703631+beeb@users.noreply.github.com>
2023-08-08 11:40:14 +0200

    update shell

--- shell.nix
+++ shell.nix
@@ -5,12 +5,11 @@
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [ nix home-manager git gnupg git-crypt pinentry ];
-    # Last command is to allow to trust the key with the `trust` command
+    nativeBuildInputs = with pkgs; [ nix home-manager git git-crypt ];
+    # After shell is open, run `gpg --edit-key 0x4592122C5C6B53B1` and the `trust` command
     shellHook = ''
       gpg --import ~/.dotfiles/home-manager/public.asc
       gpg-connect-agent "scd serialno" "learn --force" /bye
-      gpg --edit-key 0x4592122C5C6B53B1
     '';
   };
 }

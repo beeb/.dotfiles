b6f49c871df4c45302a36d2007b0dfbd750ea7c5
beeb <703631+beeb@users.noreply.github.com>
2023-08-08 13:55:44 +0200

    add user description

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -44,10 +44,7 @@
   users.users = {
     # FIXME: Replace with your username
     beeb = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      # initialPassword = "correcthorsebatterystaple";
+      description = "Valentin Bersier";
       passwordFile = config.sops.secrets.beeb_password.path;
       isNormalUser = true;
       openssh.authorizedKeys.keys = [

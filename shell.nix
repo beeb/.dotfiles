5274a50d3512ad0e5b54228453fff521e8fadda3
beeb <703631+beeb@users.noreply.github.com>
2023-08-08 11:06:04 +0200

    add pinentry

--- shell.nix
+++ shell.nix
@@ -5,10 +5,12 @@
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [ nix home-manager git gnupg git-crypt ];
+    nativeBuildInputs = with pkgs; [ nix home-manager git gnupg git-crypt pinentry ];
+    # Last command is to allow to trust the key with the `trust` command
     shellHook = ''
       gpg --import ~/.dotfiles/home-manager/public.asc
       gpg-connect-agent "scd serialno" "learn --force" /bye
+      gpg --edit-key 0x4592122C5C6B53B1
     '';
   };
 }

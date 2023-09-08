ce0f26afb11afa44df7a3925dcdc5a0854758479
beeb <703631+beeb@users.noreply.github.com>
2023-09-08 18:23:37 +0200

    add bun until it's merged into nixpkgs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, config, ... }:
+{ pkgs, inputs, outputs, config, ... }:
 {
   /* -------------------------------- overlays -------------------------------- */
   nixpkgs.overlays = [ inputs.rust-overlay.overlays.default inputs.foundry.overlay inputs.solc.overlay ];
@@ -39,6 +39,7 @@
     nil
     nixpkgs-fmt
     nodePackages.vscode-langservers-extracted
+    outputs.packages.${pkgs.system}.bun-v1
     pkgs.foundry-bin
     pkgs.solc_0_8_20
     rage
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
 { pkgs ? (import ../nixpkgs.nix) { } }: {
+  bun-v1 = pkgs.callPackage ./bun-v1 { };
   substreams = pkgs.callPackage ./substreams { };
 }

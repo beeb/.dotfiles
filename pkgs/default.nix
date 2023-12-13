69488b7056fc7111dad20306d7ee4c9d5aece503
beeb <703631+beeb@users.noreply.github.com>
2023-12-13 15:10:09 +0100

    use default espup

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
 { pkgs ? (import ../nixpkgs.nix) { } }: {
-  espup = pkgs.callPackage ./espup { };
   flowistry = pkgs.callPackage ./flowistry { };
   substreams = pkgs.callPackage ./substreams { };
   spire = pkgs.callPackage ./spire { };

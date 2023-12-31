94b0e4ee802e1adc6523c8db7e101b20bec934c0
beeb <703631+beeb@users.noreply.github.com>
2023-12-31 13:08:52 +0100

    use ssh agent from 1password

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -10,6 +10,7 @@
 
   # Forwarding the agent and so on was too much of a pain and didn't work. Using the binary from Windows solves it.
   programs.git.signing.gpgPath = "/mnt/c/Program Files (x86)/GnuPG/bin/gpg.exe";
+  programs.git.extraConfig.core.sshCommand = "ssh.exe";
 
   programs.zsh.shellAliases = {
     rt = "trash put";

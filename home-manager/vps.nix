a68827b6550cf01f929b04b33d1f0c63f31ff8b0
beeb <703631+beeb@users.noreply.github.com>
2024-06-16 22:51:03 +0200

    re-add podman

--- home-manager/vps.nix
+++ home-manager/vps.nix
@@ -3,6 +3,10 @@
   home.username = "beeb";
   home.homeDirectory = "/home/beeb";
 
+  packages = with pkgs; [
+    podman
+  ];
+
   programs.zsh = {
     envExtra = ''
       export PATH="$HOME/.cargo/bin:$PATH"

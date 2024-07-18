a7264068fd7c796de95be6485a8285555f412643
beeb <703631+beeb@users.noreply.github.com>
2024-07-18 21:14:00 +0200

    update soldeer

--- pkgs/soldeer/default.nix
+++ pkgs/soldeer/default.nix
@@ -4,16 +4,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "soldeer";
-  version = "0.2.15";
+  version = "0.2.19";
 
   src = fetchFromGitHub {
     owner = "mario-eth";
     repo = "soldeer";
     rev = "v${version}";
-    hash = "sha256-mpQ55mvXyGlWqnS1/83nxkD6cNy57NJ09qEahp55ymg=";
+    hash = "sha256-lA+ZvB5tjJzfLozUfzNCb/O7kvVb5F7yLxKOMPRIWlU=";
   };
 
-  cargoHash = "sha256-jWqHR8h+pMa6d6H7IFTJWa/dqiFndY46kroK/2CZmyg=";
+  cargoHash = "sha256-G60TDJDrL6+jXxsAq/ghYn2I5j5MN1PY9WFa0+h69jg=";
 
   doCheck = false;
 

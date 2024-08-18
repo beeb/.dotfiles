e847c393bd6068e3acb041032d368e3657224206
beeb <703631+beeb@users.noreply.github.com>
2024-08-18 10:23:43 +0200

    update to master branch

--- pkgs/yazi-master/default.nix
+++ pkgs/yazi-master/default.nix
@@ -6,25 +6,22 @@
 ,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = "yazi-master";
   version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "sxyazi";
     repo = "yazi";
-    rev = "v${version}";
-    hash = "sha256-tK2dm+WIEJGSq/PbRyagt7x43nd/o1HxP8HMj23HfnQ=";
+    rev = "99a3b3a043502a32877148515d6992e0fa121137";
+    hash = "sha256-XA5RWcmKWHyc8LSQuV/X00rh1vvvOJ+XyHCwR9Yn4K4=";
   };
 
-  # cargoLock = {
-  #   lockFile = ./Cargo.lock;
-  # };
-  cargoHash = "sha256-oD697EWa9YYFGAXFPoN0OKLmidMjZYHuD+khRh7B0/s=";
+  cargoHash = "sha256-zFLUTAR+TJrmJRhTOCLuq9cM39xO9g72Q73LmV3c7Dc=";
 
   env.YAZI_GEN_COMPLETIONS = true;
   env.VERGEN_GIT_SHA = "Nixpkgs";
-  env.VERGEN_BUILD_DATE = "2024-08-01";
+  env.VERGEN_BUILD_DATE = "2024-08-18";
 
   nativeBuildInputs = [ installShellFiles ];
   buildInputs = [ rust-jemalloc-sys ];

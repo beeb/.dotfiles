945303780bc8546ea5e71c51d0188120a4520d79
beeb <703631+beeb@users.noreply.github.com>
2025-05-16 15:37:19 +0200

    rename work laptop

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
           "valentin" = { system = "x86_64-darwin"; file = ./home-manager/macbook.nix; home = true; };
           "beeb@beebvpn" = { system = "x86_64-linux"; file = ./home-manager/vpn.nix; home = false; };
           "beeb@vps" = { system = "aarch64-linux"; file = ./home-manager/vps.nix; home = false; };
-          "beeb@beeb-Aspire-A715-51G" = { system = "x86_64-linux"; file = ./home-manager/work.nix; home = true; };
+          "beeb@pop-os" = { system = "x86_64-linux"; file = ./home-manager/work.nix; home = true; };
         };
     };
 }

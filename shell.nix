ebaf05b6f4280b1005505dfd2fd68d162423f384
beeb <703631+beeb@users.noreply.github.com>
2025-05-16 16:09:30 +0200

    add comment

--- shell.nix
+++ shell.nix
@@ -7,6 +7,7 @@
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [ nix home-manager git git-crypt ];
     # After shell is open, run `gpg --edit-key 0x4592122C5C6B53B1` and the `trust` command
+    # Pre-requirement: sudo apt install scdaemon
     shellHook = ''
       gpg --import ~/.dotfiles/pubkeys/public.asc
       gpg-connect-agent "scd serialno" "learn --force" /bye

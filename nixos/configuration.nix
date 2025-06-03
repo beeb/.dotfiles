2655507c7061eb9199754fe0397ed21ea18aabf5
Valentin B. <703631+beeb@users.noreply.github.com>
2025-06-03 12:11:08 +0200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,7 +81,7 @@
     users = {
       beeb = {
         description = "Valentin";
-        passwordFile = config.sops.secrets.beeb_password.path;
+        # passwordFile = config.sops.secrets.beeb_password.path;
         isNormalUser = true;
         openssh.authorizedKeys.keys = [
           # TODO: SSH public key(s) here, if you plan on using SSH to connect

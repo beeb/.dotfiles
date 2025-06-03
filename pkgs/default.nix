ef4532869e8c51aaa4855437b60ee112a4f35a0d
Valentin B. <703631+beeb@users.noreply.github.com>
2025-06-03 11:28:46 +0200

    fix pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
-{ pkgs ? (import ../nixpkgs.nix) { } }: {
+pkgs: {
   crates-lsp = pkgs.callPackage ./crates-lsp { };
   myNodePackages = pkgs.callPackage ./myNodePackages { };
   scls = pkgs.callPackage ./scls { };
